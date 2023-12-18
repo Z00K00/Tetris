bacc4473af9a7272e099dce2153cefd80ae8aec0
kraiglarson2 <kraigalarson2@gmail.com>
2023-12-17 23:00:31 -0500

    Initial Commit

---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by Krajg Larson on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
